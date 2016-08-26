5518a9dc5fd5cab29bf4327fb3ef3b7b96ea3f5a
Matt Palmer <matt@silvercreekit.com>
2016-08-26 01:15:44 -0400

    Move all significant code in buildConfig outside of index

--- buildConfig/helpers.js
+++ buildConfig/helpers.js
@@ -1,7 +1,52 @@
+const path = require('path');
 const webpack = require('webpack');
+const Html = require('html-webpack-plugin');
+const declarations = require('./declarations');
+
+function devServer() {
+  return {
+    plugins: [new webpack.HotModuleReplacementPlugin({ multiStep: true })],
+    devServer: {
+      inline: true,
+      hot: true,
+      historyApiFallback: true,
+      stats: 'errors-only',
+    },
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  };
+}
+
+function generateHtml() {
+  return {
+    plugins: [
+      new Html({
+        title: 'Example project',
+        template: path.join(declarations.paths.src, 'template.html'),
+      }),
+    ],
+  };
+}
+
+function optimizeForProd() {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { drop_console: true },
+        mangle: {
+          except: ['webpackJsonp'],
+          screw_ie8: true,
+        },
+      }),
+      new webpack.optimize.DedupePlugin(),
+    ],
+  };
+}
 
 function extractBundle(filename, names) {
   return { plugins: [new webpack.optimize.CommonsChunkPlugin({ filename, names })] };
 }
 
-module.exports = { extractBundle };
+module.exports = { devServer, generateHtml, optimizeForProd, extractBundle };
--- buildConfig/index.js
+++ buildConfig/index.js
@@ -1,55 +1,6 @@
-const path = require('path');
-const webpack = require('webpack');
-const Html = require('html-webpack-plugin');
 const declarations = require('./declarations');
 const bundles = require('./bundles');
 const helpers = require('./helpers');
 const base = require('./base');
 
-function devServer() {
-  return {
-    plugins: [new webpack.HotModuleReplacementPlugin({ multiStep: true })],
-    devServer: {
-      inline: true,
-      hot: true,
-      historyApiFallback: true,
-      stats: 'errors-only',
-    },
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000,
-    },
-  };
-}
-
-function generateHtml() {
-  return {
-    plugins: [
-      new Html({
-        title: 'Example project',
-        template: path.join(declarations.paths.src, 'template.html'),
-      }),
-    ],
-  };
-}
-
-function optimizeForProd() {
-  return {
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: { drop_console: true },
-        mangle: {
-          except: ['webpackJsonp'],
-          screw_ie8: true,
-        },
-      }),
-      new webpack.optimize.DedupePlugin(),
-    ],
-  };
-}
-
-module.exports = Object.assign(base, declarations, bundles, helpers, {
-  devServer,
-  generateHtml,
-  optimizeForProd,
-});
+module.exports = Object.assign(base, declarations, bundles, helpers);
